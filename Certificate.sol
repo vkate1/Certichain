9cd2eb0da5ef97aef064a73d64313212e0739e87
kole-swapnil <60402552+kole-swapnil@users.noreply.github.com>
2020-02-06 01:18:30 +0530

    Update Certificate.sol

--- Certificate.sol
+++ Certificate.sol
@@ -1,5 +1,3 @@
-
-
 pragma solidity ^0.5.0;
 
 contract Certificates{
@@ -10,6 +8,7 @@ contract Certificates{
         string fname;
         string lname;
         string thash;
+        uint256 time;
         
     }
     
@@ -18,13 +17,15 @@ contract Certificates{
     
     function addStudent(string memory f_name,string memory l_name,uint256 no)public{
         Transactid++;
-        student[Transactid] = Student(no,f_name,l_name,'hash');
+        student[Transactid] = Student(no,f_name,l_name,'hash',0);
         
     }
     
     function addHash(uint256 Transact_id,string memory hash)public{
         require(Transact_id > 0 && Transact_id <= Transactid);
         student[Transact_id].thash = hash;
+        student[Transact_id].time = now;
+        
         emit add(Transact_id);
     }
     
@@ -33,3 +34,4 @@ contract Certificates{
     }
     
 }
+
